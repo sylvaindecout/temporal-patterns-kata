5683d5eb996d2f53ce6de4ac762bcac896e08c64
Sylvain Decout <sylvain.decout+github@gmail.com>
2023-06-16 18:43:24 +0200

    :test_tube: Temporal object - Test

--- src/test/kotlin/fr/sdecout/dddeurope/supply/SupplyPlanningTest.kt
+++ src/test/kotlin/fr/sdecout/dddeurope/supply/SupplyPlanningTest.kt
@@ -3,6 +3,7 @@ package fr.sdecout.dddeurope.supply
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
 import java.time.DayOfWeek.*
+import java.time.Instant
 import java.time.LocalDate
 
 class SupplyPlanningTest : ShouldSpec({
@@ -51,6 +52,73 @@ class SupplyPlanningTest : ShouldSpec({
                 LocalDate.parse("2023-01-20") to dailyDemands,
                 LocalDate.parse("2023-01-23") to dailyDemands,
                 LocalDate.parse("2023-01-25") to dailyDemands
+            )
+        }
+    }
+
+    context("Testing the demand calculation with changes during time") {
+
+        should("calculate total demands of period") {
+            val formerPlan = SupplyPlan(
+                timestamp = Instant.parse("2023-01-01T10:00:00Z"),
+                title = "Weekly Fruit Supply",
+                activePeriod = LocalDate.parse("2023-01-01")..LocalDate.parse("2023-12-31"),
+                scheduleDays = setOf(MONDAY, WEDNESDAY, FRIDAY),
+                demandEntries = mapOf(
+                    "Apple" to Quantity(50),
+                    "Banana" to Quantity(30)
+                )
+            )
+            val updatedPlan = formerPlan.changePlan(
+                scheduleDays = setOf(TUESDAY, THURSDAY),
+                demandEntries = mapOf(
+                    "Apple" to Quantity(30),
+                    "Orange" to Quantity(40)
+                ),
+                timestamp = Instant.parse("2023-02-01T10:00:00Z")
+            )
+
+            val demands = updatedPlan.totalDemandsIn(LocalDate.parse("2023-01-20")..LocalDate.parse("2023-02-10"))
+
+            demands shouldBe mapOf(
+                "Apple" to Quantity(340),
+                "Banana" to Quantity(150),
+                "Orange" to Quantity(120)
+            )
+        }
+
+        should("calculate demands of days in a period") {
+            val dailyDemands = mapOf(
+                "Apple" to Quantity(50),
+                "Banana" to Quantity(30)
+            )
+            val formerPlan = SupplyPlan(
+                timestamp = Instant.parse("2023-01-01T10:00:00Z"),
+                title = "Weekly Fruit Supply",
+                activePeriod = LocalDate.parse("2023-01-01")..LocalDate.parse("2023-12-31"),
+                scheduleDays = setOf(MONDAY, WEDNESDAY, FRIDAY),
+                demandEntries = dailyDemands
+            )
+            val newDailyDemands = mapOf(
+                "Orange" to Quantity(4)
+            )
+            val updatedPlan = formerPlan.changePlan(
+                scheduleDays = setOf(TUESDAY, THURSDAY),
+                demandEntries = newDailyDemands,
+                timestamp = Instant.parse("2023-02-01T10:00:00Z")
+            )
+
+            val demands = updatedPlan.demandsIn(LocalDate.parse("2023-01-20")..LocalDate.parse("2023-02-10"))
+
+            demands shouldBe mapOf(
+                LocalDate.parse("2023-01-20") to dailyDemands,
+                LocalDate.parse("2023-01-23") to dailyDemands,
+                LocalDate.parse("2023-01-25") to dailyDemands,
+                LocalDate.parse("2023-01-27") to dailyDemands,
+                LocalDate.parse("2023-01-30") to dailyDemands,
+                LocalDate.parse("2023-02-02") to newDailyDemands,
+                LocalDate.parse("2023-02-07") to newDailyDemands,
+                LocalDate.parse("2023-02-09") to newDailyDemands
             )
         }
     }
